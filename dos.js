5d1f717ec331b938e836bb4a93d8617f6819d746
natt-toc <60528994+natt-toc@users.noreply.github.com>
2020-08-10 20:34:19 -0300

    Add files via upload
    dos.js

--- dos.js
+++ dos.js
@@ -1,16 +1,21 @@
 /*
 2)De una compra debes ingresar una cantidad indeterminada de productos,
  
-a)informar el peso total de la compra.
-b)la marca del más caro de los líquidos
-c)la marca del más barato de los sólidos
 */function mostrar() {
+
+  let respuesta;
   let marca;
   let precio;
   let peso;
-  let tipo;
   let acumuladorpeso = 0;
-
+  let tipo;
+  let flagLiq = 0;
+  let marcaLiqCaro;
+  let precioMarcaLiqCara
+  let flagSol = 0;
+  let precioMasBaratoSolido;
+  let marcaMasCaroLiq;
+  let marcaBaratoSol;
 
   do {
 
@@ -29,21 +34,57 @@ c)la marca del más barato de los sólidos
     while (isNaN(precio));
 
     //peso en Kg
-
     do {
       peso = parseInt(prompt("ingrese el peso"));
+      acumuladorpeso += peso
     }
     while (isNaN(peso));
 
     // tipo
-
     do {
       tipo = prompt("Ingrese el tipo: Sólido o líquido");
     }
     while (!(tipo == "Sólido" || tipo == "líquido"));
 
+    //a) informar el peso total de la compra. En el acumulador "acumuladorpeso"
 
-  }
+    // b) la marca del más caro de los líquidos
+
+    if (tipo == "líquido") {
+
+      if (flagLiq == 0) {
+        marcaLiqCaro = marca;
+        precioMarcaLiqCara = precio;
+        flagLiq = 1;
+
+      } 
+       else if (precioMarcaLiqCara > precio) {
+        precioMarcaLiqCara = precio;
+        marcaMasCaroLiq=marca
+      }
+       } else if (tipo == "Sólido") {
+
+        if (flagSol == 0) {
+          marcaSolidoBarata = marca;
+          precioMasBaratoSolido = precio;
+          flagSol = 1;
+
+        } else if (precioMasBaratoSolido < precio) {
+          precioMasBaratoSolido = precio;
+          marcaBaratoSol=marca;
+        }
+
+      }
+
+      respuesta= confirm("Desea continuar??");
+
+    
+    
+    } while (respuesta == true);
 
 
+
+  document.write(" El peso total de la compra es de " + acumuladorpeso + "<br/>");
+  document.write("La marca del más caro de los líquidos es " + marcaMasCaroLiq + "<br/>");
+  document.write("la marca del más barato de los sólidos " + marcaBaratoSol + "<br/>");
 }
