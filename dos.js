ed876fc81dd27160f242e93391c1294193a203f4
natt-toc <60528994+natt-toc@users.noreply.github.com>
2020-08-10 19:51:52 -0300

    Add files via upload
    en curso dos.js

--- dos.js
+++ dos.js
@@ -1,11 +1,49 @@
 /*
 2)De una compra debes ingresar una cantidad indeterminada de productos,
- validando los siguientes datos:
-marca, precio, peso en kilogramos, tipo(sólido o líquido)
+ 
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/function mostrar() {
+  let marca;
+  let precio;
+  let peso;
+  let tipo;
+  let acumuladorpeso = 0;
+
+
+  do {
+
+
+    //  validar os siguientes datos: marca, precio, peso en kilogramos, tipo(sólido o líquido)
+    do {
+      // marca
+      marca = prompt("Ingrese la marca deseada ")
+
+    } while (!(isNaN(marca)));
+
+    // precio
+    do {
+      precio = parseInt(prompt("Ingrese el precio"));
+    }
+    while (isNaN(precio));
+
+    //peso en Kg
+
+    do {
+      peso = parseInt(prompt("ingrese el peso"));
+    }
+    while (isNaN(peso));
+
+    // tipo
+
+    do {
+      tipo = prompt("Ingrese el tipo: Sólido o líquido");
+    }
+    while (!(tipo == "Sólido" || tipo == "líquido"));
+
+
+  }
+
+
 }
